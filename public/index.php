df5925dd1064fc11ff7484b63c046261d2bdd650
Tara Byassee <tarabyassee@students.abtech.edu>
2024-02-05 20:09:02 -0500

    first commit

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Asheville Tailgate Market</title>
+  </head>
+  
+  <body>
+    <h1>Asheville Tailgate Market</h1>
+  </body>
+</html>
---
+++ public/staff/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Staff Index</title>
+    <link rel="stylesheet" media="all" href="../stylesheets/staff.css">
+
+  </head>
+  
+  <body>
+    <header>
+      <h1>Staff Area</h1>
+    </header>
+    <nav>
+      <ul>
+        <li><a href="index.php">Menu</a></li>
+      </ul>
+    </nav>
+    <footer>
+      &copy; <?php echo date('Y')?> TLB Products
+    </footer>
+  </body>
+</html>
